f072dbbdcb7780d872c82ca81caaca26d39a6c12
wszuc <wiktor.szuca@gmail.com>
2025-01-19 15:53:58 +0000

    styles added

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express();
 const router = express.Router();
-
-const path = __dirname + '/views/'
+const path = require('path')
 const port = 8080;
+const morgan = require('morgan')
 
-app.get('/', (req, res) => { res.send('Hello World!') })
+app.use(morgan('tiny'))
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.get('/', (req, res) => { 
+    res.sendFile(path.join(__dirname, '/public/static/index.html')) 
+})
 
 app.listen(port, () => {
     console.log("App listening on port ", port)
